f947c27af455adf2187dddd1f4c7a373e615fc5d
Rick Cooper <rick.cooper@upside.energy>
2022-05-27 22:42:32 +0100

    fix: prettier on json

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 import toDateDiff from '../src/index';
 
 test('thing', () => {
-  expect(toDateDiff('2022-05-25Z')).toEqual(DateTime.fromISO('2022-05-25Z'));
+  expect(toDateDiff('2022-05-25Z').toISODate()).toEqual(DateTime.fromISO('2022-05-25Z').toISODate());
 });
 
 test('thing2', () => {
