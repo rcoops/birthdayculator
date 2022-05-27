13b6db9ae0953ac95ee9f30dfb1a516a5e21388c
Rick Cooper <rick.cooper@upside.energy>
2022-05-27 19:55:11 +0100

    chore: tests

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -2,5 +2,9 @@ import { DateTime } from 'luxon';
 import { toDateDiff } from '../src/index';
 
 test('thing', () => {
-  expect(toDateDiff('2022-05-25')).toEqual(DateTime.fromISO('2022-05-25'))
+  expect(toDateDiff('2022-05-25Z')).toEqual(DateTime.fromISO('2022-05-25Z'))
+});
+
+test('thing2', () => {
+  expect(toDateDiff(DateTime.fromISO('2022-05-25Z'))).toEqual(DateTime.fromISO('2022-05-25Z'))
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import {DateTime} from 'luxon';
-interface X {
-  x: 1;
+
+
+function toDateTime(date: string | DateTime) {
+  if (typeof date === "string") {
+    return DateTime.fromISO(date, {zone: 'utc'});
+  }
+  return date;
 }
 
-const y: X = {x: 1}
-console.log(DateTime.fromISO("2022-05-27T00:00:00Z"));
-
-
-export function toDateDiff(date: string | DateTime) {
-  if (typeof date === "string") {
-    return DateTime.fromISO(date);
-  }
-  return date
+export function toDateDiff(date: string | DateTime, other?: string | DateTime) {
+  date = toDateTime(date);
+  other = other === undefined ? DateTime.utc() : toDateTime(other);
+  return date;
 }
