652137e860478bbdf9473f88ebd2a5c91bc45a97
Rick Cooper <rick.cooper@upside.energy>
2022-05-28 06:39:20 +0100

    temp lower coverage

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,13 @@ module.exports = {
       tsconfig: './tsconfig.test.json',
     },
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    }
+  }
 };
